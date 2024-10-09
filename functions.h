497cd16ae45fbcd44719e532ae55b83f8c40b294
Alvin Majawa_166876 <alvinmajawa1@gmail.com>
2024-10-09 07:39:28 +0300

    Completion code

---
+++ functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void generateFixtures();
+void scheduleMatches();
+void addStadiumInfo();
+void createFinalSchedule();
+
+#endif
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <random>
 #include <string>
 
+#include "functions.h"
+
 using namespace std;
 
 // Function to get the number of weekends available in the file
@@ -53,7 +55,56 @@ void displayScheduledWeekendMatches(bool displayAll, int selectedWeekend = -1) {
     scheduleFile.close();
 }
 
-int main()
-{
-    //Main function run here
+int main() {
+    generateFixtures();
+    scheduleMatches();
+    addStadiumInfo();
+    string filePath = "../updatedScheduledWeekends.csv";
+
+    int totalWeekends = getNumberOfWeekends(filePath);
+
+    if (totalWeekends == 0) {
+        cout << "No weekends available to display." << endl;
+        return 1;
+    }
+
+    while (true) {
+        cout << "\nWould you like to see all weekend matches or choose a specific weekend? \n(Enter 'a or A' For 'All' and 's or S' for 'Specific'): ";
+        string userChoice;
+        cin >> userChoice;
+
+        if (userChoice == "A" || userChoice == "a" ) {
+            displayScheduledWeekendMatches(true);
+        } else if (userChoice == "S" || userChoice == "s" ) {
+            cout << "\nThere are " << totalWeekends << " weekends available. ";
+            cout << "Please enter a weekend number between 1 and " << totalWeekends << ".\nInput: ";
+            int weekendNumber;
+            cin >> weekendNumber;
+
+            if (weekendNumber >= 1 && weekendNumber <= totalWeekends) {
+                displayScheduledWeekendMatches(false, weekendNumber);
+            } else {
+                cout << "Invalid weekend number. Please try again.";
+                continue;
+            }
+        }
+        else {
+            cout << "Invalid choice. Please enter 'a or A' For 'All' and 's or S' for 'Specific'.";
+            continue;
+        }
+
+        cout << "\nWould you like to pick another weekend, see all weekends, or end the program? \n(Please enter any number or letter to conntinue or Enter 'end - To close Program')\nInput: ";
+        string nextAction;
+        cin >> nextAction;
+        cout << "\n___________________________________________________________________________________________________";
+
+        if (nextAction == "end") {
+            cout << "\n\t\tExiting the program. Thank you for accessing our system, Goodbye!\n";
+            cout << "___________________________________________________________________________________________________";
+
+            break;
+        }
+    }
+
+    return 0;
 }
